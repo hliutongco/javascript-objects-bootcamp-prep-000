72dc08fd045606ac26001064250e9f4333504b85
Helen Liutongco <bellpickle@gmail.com>
2017-10-25 05:10:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist,artistName,songTitle) {
 //  playlist[artistName]=songTitle;
   return Object.assign(playlist,{[artistName]: songTitle});
 }
+
+function removeFromPlaylist() {
+  return delete playlist.artistName;
+}
