534993727af0300ffd0121adb7750085e854ba97
Helen Liutongco <bellpickle@gmail.com>
2017-10-25 05:04:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = new Object({KDot: "F*** Your Ethnicity"});
 
 function updatePlaylist(playlist,artistName,songTitle) {
 //  playlist[artistName]=songTitle;
-  return Object.assign(playlist,{kDot: "F*** Your Ethnicity"},{artistName: songTitle});
+  return Object.assign(playlist,{KDot: "F*** Your Ethnicity"},{artistName: songTitle});
 }
